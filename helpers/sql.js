3504e6462d70be7dd7f6443adf4336b2f85d77ef
Phill Legnitto <palegnitto@gmail.com>
2022-08-24 11:44:19 -0700

    Docstring sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,6 +2,23 @@ const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
+/**
+ * Accepts an Object (dataToUpdate) which can partially update rows in db,
+ *  and an Object jsToSQL which maps javascript keys to psql columns.
+ *
+ * Data to update = {name, description, numEmployees, logoUrl}
+ *
+ * jsToSql = { keyforupdate: value});
+ *
+ * Example return:
+ *
+ *  {
+ *  setCols: '"first_name"=$1', '"age"=$2'
+ *  values:[`dataToUpdate_values`]
+ * }
+ *
+*/
+
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
